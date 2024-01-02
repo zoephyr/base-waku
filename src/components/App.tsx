68ef2833cf4ab531f11d04ec13fe255d10cb2dc5
Zoey <38047633+uerk-io@users.noreply.github.com>
2024-01-02 11:48:51 -0500

    Add Tailwind base config and styling to home example

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,9 +3,9 @@ import { Counter } from './Counter.js';
 const App = ({ name }: { name: string }) => {
   return (
     <div style={{ border: '3px red dashed', margin: '1em', padding: '1em' }}>
-      <h1 className="text-3xl font-bold underline text-red-500">Hello {name}!!</h1>
-      <h3>This is a server component. I guess this is going to be the rage this year</h3>
-      <p>I sure hope I bet right on this technology :^^^))</p>
+      <h1 className="text-3xl font-bold underline text-red-500">Hello {name}!</h1>
+      <h3 className="text-xl font-bold underline text-purple-500">This is a server component. I guess this is going to be the rage this year</h3>
+      <p>I sure hope I bet right on this technology...</p>
       <Counter />
     </div>
   );
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -6,8 +6,8 @@ export const Counter = () => {
   const [count, setCount] = useState(0);
   return (
     <div style={{ border: '3px blue dashed', margin: '1em', padding: '1em' }}>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+      <p><strong>Count</strong>: {count}</p>
+      <button className="border-blue-500 p-1 rounded-md border-4" onClick={() => setCount((c) => c + 1)}>Increment</button>
       <h3>This is a client component. I'd sure love to understand RSC interleaving components more....</h3>
     </div>
   );
